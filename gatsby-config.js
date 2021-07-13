bc6d6ae1e49137bab023567799116fd2a71ec9d8
Peter Sekesan <psekesan@gmail.com>
2021-07-13 16:53:05 -0400

    initial meta commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Flexible Gatsby`,
-    description: `I am a web developer focusing on front-end development. Always hungry to keep learning.`,
-    author: `David Freeman`,
+    title: `Peter Sekesan`,
+    description: `I am a software engineer focusing on all things JavaScript development at the moment. Always hungry to keep learning.`,
+    author: `Peter Sekesan`,
     siteUrl: `https://github.com/wangonya/flexible-gatsby`,
     social: {
-      twitter: `wang0nya`,
+      twitter: `petersekesan`,
       facebook: ``,
-      github: `wangonya`,
-      linkedin: `wangonya`,
-      email: `kwangonya@gmail.com`,
+      github: `Pete-Sekesan`,
+      linkedin: `petersekesan`,
+      email: `psekesan@gmail.com`,
     },
   },
   plugins: [
@@ -35,8 +35,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 970,
-	      withWebp: true,
-	      withAvif: true,
+              withWebp: true,
+              withAvif: true,
             },
           },
           {
